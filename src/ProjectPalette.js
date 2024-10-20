4f70adbf8b8d821ba82e73d6b54188e8430c068d
Arvind K <arvindk@arvinds-air.home>
2024-10-20 20:38:42 +0100

    Fixed link issue

--- src/ProjectPalette.js
+++ src/ProjectPalette.js
@@ -1,33 +1,31 @@
-import React from 'react'
-import { makeStyles} from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import { Link } from 'react-router-dom';
-import Typography from '@material-ui/core/Typography';
-import {withThemeContext} from "./ThemeProvider"
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import { Link } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
+import { withThemeContext } from "./ThemeProvider";
 const useStyles = makeStyles((theme) => ({
-    root:(props)=> {
-      return{
-        backgroundColor:props.theme.themeProperties.foregroundColor,
-        color:props.theme.themeProperties.color,
-      }
-    
-      },
-      link:{
-        textDecoration:"none" 
-
-      }
-}))
-function ProjectPalette(props){
-  const {paletteData}=props;
+  root: (props) => {
+    return {
+      backgroundColor: props.theme.themeProperties.foregroundColor,
+      color: props.theme.themeProperties.color,
+    };
+  },
+  link: {
+    textDecoration: "none",
+  },
+}));
+function ProjectPalette(props) {
+  const { paletteData } = props;
   const classes = useStyles(props);
-   
-    return(
-        <Card className={classes.root}>
-        <CardActionArea>
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
         <Link to={`/projects/${paletteData.projectName}`}>
           <CardMedia
             component="img"
@@ -35,32 +33,66 @@ function ProjectPalette(props){
             height="250"
             image={paletteData.projectThumbnail}
             title={paletteData.projectName}
-          
           />
-          </Link>
+
           <CardContent>
-            <Typography gutterBottom variant="h5" >
+            <Typography gutterBottom variant="h5">
               {paletteData.projectName}
             </Typography>
-            <Typography variant="body2" color="textSecondary" component="p"style={{color:props.theme.theme==="light theme"?"rgba(0, 0, 0, 0.54)":props.theme.themeProperties.otherColor}}>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+              style={{
+                color:
+                  props.theme.theme === "light theme"
+                    ? "rgba(0, 0, 0, 0.54)"
+                    : props.theme.themeProperties.otherColor,
+              }}
+            >
               {paletteData.shortDescription}
             </Typography>
-            <Typography variant="body1" color="textSecondary" component="p"style={{color:props.theme.theme==="light theme"?"rgba(0, 0, 0, 0.54)":props.theme.themeProperties.otherColor,marginTop:"10px"}}>
+            <Typography
+              variant="body1"
+              color="textSecondary"
+              component="p"
+              style={{
+                color:
+                  props.theme.theme === "light theme"
+                    ? "rgba(0, 0, 0, 0.54)"
+                    : props.theme.themeProperties.otherColor,
+                marginTop: "10px",
+              }}
+            >
               {`Responsive: ${paletteData.responsive}`}
             </Typography>
-            <Typography variant="body1" color="textSecondary" component="p"style={{color:props.theme.theme==="light theme"?"rgba(0, 0, 0, 0.54)":props.theme.themeProperties.otherColor}}>
+            <Typography
+              variant="body1"
+              color="textSecondary"
+              component="p"
+              style={{
+                color:
+                  props.theme.theme === "light theme"
+                    ? "rgba(0, 0, 0, 0.54)"
+                    : props.theme.themeProperties.otherColor,
+              }}
+            >
               {`Best viewed in: ${paletteData.compatibleDevices}`}
             </Typography>
           </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <a href={paletteData.url} className={classes.link} target="_blank" rel="noreferrer">
-            View
-          </a>
-        </CardActions>
-      </Card>
-        
-    )
-
+        </Link>
+      </CardActionArea>
+      <CardActions>
+        <a
+          href={paletteData.url}
+          className={classes.link}
+          target="_blank"
+          rel="noreferrer"
+        >
+          View
+        </a>
+      </CardActions>
+    </Card>
+  );
 }
-export default withThemeContext(ProjectPalette)
+export default withThemeContext(ProjectPalette);
